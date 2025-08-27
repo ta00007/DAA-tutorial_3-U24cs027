0f7ac98647ebc186954f71a3c1258e2504d85c96
ta00007 <tarun48581@gmail.com>
2025-08-27 09:53:33 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    vector<int> A = {7, 10, 4, 3, 20, 15, 8, 17, 5, 1,5,34,5,65,7,5,7,5,6,5,4,32,54,6,7,8,67,6,5,4,,433,4,6,7,8,6,4,4,3};
+    vector<int> A = {7, 10, 4, 3, 20, 15, 8, 17, 5, 1};
     int k = 3;
 
     priority_queue<int, vector<int>, greater<int>> minHeap(A.begin(), A.end()); 
@@ -13,3 +13,4 @@ int main() {
 
     cout << minHeap.top() << endl;
 }
+
